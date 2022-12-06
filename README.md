38680910bd5491bf58da293742a75b30e3931b6b
crispim <crispim1411@gmail.com>
2022-12-06 10:06:14 -0300

    true setState

--- README.md
+++ README.md
@@ -3,8 +3,12 @@ Aplicação feita em Flutter implementando diversas abordagens para gerenciament
 Entre eles, setState, Provider, BLoC,... (incluir conforme for implementando)
 As implementações foram separadas na estrutura do aplicativo por rotas.
 
-Os dados são requisitados de uma api pública que serve números randômicos.
+Os dados são requisitados de uma api pública que serve números randômicos. O objetivo é ter chamadas regulares atualizando o valor na tela. Utilizando **Time.periodic**
+```
+Timer.periodic(const Duration(seconds: 2), getValue());
+```
 
 ## Abordagens
 ### setState
-...
+[Implementação](https://github.com/crispim1411/state_management/tree/master/lib/setState)
+A lógica do elemento encontra total ou parcialmente dentro da implementação da interface. 
--- lib/setState/setstate_screen.dart
+++ lib/setState/setstate_screen.dart
@@ -1,30 +1,49 @@
+import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import '../config.dart';
 
-import 'api_connector.dart';
-
-class SetStateScreen extends StatelessWidget {
+class SetStateScreen extends StatefulWidget {
   const SetStateScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _SetStateScreenState();
+}
+
+class _SetStateScreenState extends State<SetStateScreen> {
+  String screenValue = 'loading...';
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(const Duration(seconds: 2), (_) => getValue());
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('setState Approach')),
-      body: _displayValue()
+      body: Center(child: Text(screenValue))
     );
   }
 
-  _displayValue() {
-    return Center(
-      child: FutureBuilder(
-        future: ApiConnector.getValue(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return Text(snapshot.data as String);
-          } else {
-            return const CircularProgressIndicator();
-          }
-        }
-      ),
-    );
+  void getValue() async {
+    var uri = Uri.http(Config.apiURL, Config.endpoint);
+    var response = await http.get(uri);
+    var value = jsonDecode(response.body)[0];
+    
+    if (mounted) {
+      setState(() {
+        screenValue = value;
+      });
+    }
   }
 }
